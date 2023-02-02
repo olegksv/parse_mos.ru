62e6e89c72c926154c9b702fc53ff43f540c062a
olegksv <120097309+olegksv@users.noreply.github.com>
2023-02-02 19:48:53 +0300

    Update settings.py

--- django_selenium/django_selenium/settings.py
+++ django_selenium/django_selenium/settings.py
@@ -29,15 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'snegir8881@gmail.com'
-EMAIL_HOST_PASSWORD = 'rngx eymi mxpt ygww'
 
 
 # Application definition
